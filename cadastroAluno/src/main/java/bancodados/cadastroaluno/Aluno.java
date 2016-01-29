9a3523fff80779163364dd102733f3801f2b9702
carlosataide <carlos-andre@hotmail.com.br>
2016-01-29 20:48:57 -0200

    Update Aluno.java

--- cadastroAluno/src/main/java/bancodados/cadastroaluno/Aluno.java
+++ cadastroAluno/src/main/java/bancodados/cadastroaluno/Aluno.java
@@ -73,6 +73,38 @@ public class Aluno {
     }
   }
   
+  public static List<Aluno> listar() {  
+    List<Aluno> alunos = new ArrayList<Aluno>();
+      try {
+      //Obtém a conexão.
+      String url = "jdbc:derby:C:\\banco-de-teste;create=true";
+      Connection conn = DriverManager.getConnection(url);
+      //Cria a sentença SQL.
+      String sql = "insert into aluno (matricula, nome, fone, cpf) values (?, ?, ?, ?)";
+      //Obtém referência para uma sentença SQL.
+      PreparedStatement prepareStatement = conn.prepareStatement(sql);
+      ResultSet rs = prepareStatement.executeQuery();
+      while(rs.next()){
+          Aluno a = new Aluno();
+          a.setMatricula(rs.getString("matricula"));
+          a.setNome(rs.getString("nome"));
+          a.setFone(rs.getString("fone"));
+          a.setCpf(rs.getString("cpf"));
+          
+          alunos.add(a);
+          
+          rs.close();
+      }
+       
+      rs.close();
+      conn.close();
+    } catch(Throwable e) {
+      //Para repassar a exceção para o container tratar.
+      throw new RuntimeException(e);
+    }
+      return alunos;
+  }
+  
    public void alterar() {
     try {
       //Obtém a conexão.
